40d502a7815bdae1b9820528b0f792b91c8147a4
jonny <sanyaplesh@gmail.com>
2022-03-09 18:56:48 +0300

    Lesson1_2

---
+++ Lesson1_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запросить у пользователя минимальную и максимальную температуру за сутки
+            Console.WriteLine("Пожалуйста напишите минимальную температуру за сутки");
+            string mintemp = Console.ReadLine();
+
+            Console.WriteLine("Пожалуйста напишите максимальную температуру за сутки");
+            string maxtemp = Console.ReadLine();
+            
+            int temp1 = Convert.ToInt32(mintemp);
+            int temp2 = Convert.ToInt32(maxtemp);
+
+            int result = (temp1 + temp2) / 2;
+
+            Console.WriteLine("Средняя суточная температура");
+            Console.WriteLine(result);
+
+        }
+    }
+}
